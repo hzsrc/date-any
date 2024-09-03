b5a6f7b0fe033f78e769a4e1cc3d797c2abf4434
hzsrc <hz8889999>
2024-09-03 11:48:01 +0800

    fix month

--- toDate.js
+++ toDate.js
@@ -3,7 +3,7 @@ module.exports = function toDate(date0, force) {
     if (typeof date0 === 'string') {
         var m = /(\d+)([\-\/])(\d+)\2(\d+)$/.exec(date0)
         if (m) { //2022-02-02 变成 2022-02-02 08:00:00的问题
-            return new Date(m[1], m[3], m[4])
+            return new Date(m[1], m[3] - 1, m[4])
         }
         var d = new Date(date0);
         if (!isNaN(d)) return d
